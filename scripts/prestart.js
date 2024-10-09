ee1f186a324714c3859c19dff791d1078b49b71d
Matthew Oliveira <m4olivei@gmail.com>
2024-10-09 10:42:11 -0400

    feat: adjust to using package imports for easy npm link local testing

--- scripts/prestart.js
+++ scripts/prestart.js
@@ -1,28 +1,21 @@
 import { glob } from "glob";
 import { writeFile } from "fs/promises"
 
-const cibm = "https://1.www.s81c.com/common/carbon-for-ibm-dotcom/version";
-const cwc = "https://1.www.s81c.com/common/carbon/web-components/version";
+const cibm = "@carbon/ibmdotcom-web-components/es/components"
+const cwc = "@carbon/web-components/es/components";
 
 (async () => {
-  const cibm_pkg = await fetch('https://registry.npmjs.org/@carbon/ibmdotcom-web-components').then(r => r.json());
-  const cibm_version = cibm_pkg["dist-tags"].latest;
-
   const cibm_components = await glob('./node_modules/@carbon/ibmdotcom-web-components/es/components/*/index.js');
   const cibm_imports = cibm_components
     .map(path => path.split('/').at(-2))
-    .map(cmp => `import "${cibm}/v${cibm_version}/${cmp}.min.js";`)
+    .map(cmp => `import "${cibm}/${cmp}/index.js";`)
     .join('\n');
-
-
-  const cwc_pkg = await fetch('https://registry.npmjs.org/@carbon/web-components').then(r => r.json());
-  const cwc_version = cwc_pkg["dist-tags"].latest;
 
   const cwc_components = await glob('./node_modules/@carbon/web-components/es/components/*/index.js');
   const cwc_imports = cwc_components
     .map(path => path.split('/').at(-2))
-    .map(cmp => `import "${cwc}/v${cwc_version}/${cmp}.min.js";`)
+    .map(cmp => `import "${cwc}/${cmp}/index.js";`)
     .join('\n');
 
-  await writeFile('components.js', [...cwc_imports, ...cibm_imports]);
+  await writeFile('components.js', [...cwc_imports, "\n", ...cibm_imports]);
 })();
