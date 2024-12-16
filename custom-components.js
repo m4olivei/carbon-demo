1d367697f763d062943879cf9ef3d7071f6f38d0
Matthew Oliveira <m4olivei@gmail.com>
2024-12-16 11:50:16 -0500

    setup a demo showin native class fields error

---
+++ custom-components.js
@@ -0,0 +1,5 @@
+import "@carbon/ibmdotcom-web-components/es/components/masthead/index.js";
+import "@carbon/ibmdotcom-web-components/es/components/cta/index.js";
+import "@carbon/ibmdotcom-web-components/es/components/cta/cta.js";
+import "@carbon/ibmdotcom-web-components/es/components/button-group/index.js";
+import "@carbon/ibmdotcom-web-components/es/components/button/index.js";
